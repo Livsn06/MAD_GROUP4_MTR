38fd4c1772dce628edfcf1889138f618e6b92f6f
Livsn06 <joelgutlau04@gmail.com>
2024-03-23 20:01:56 +0800

    database created

--- lib/providers/letter_functions.dart
+++ lib/providers/letter_functions.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: non_constant_identifier_names, unused_field
+
 import 'package:flutter/material.dart';
 import 'package:gutlay_etr_mad/data/levels.dart';
+import 'package:gutlay_etr_mad/helpers/dbase_hive.dart';
 import 'package:gutlay_etr_mad/model/levels/level_model.dart';
 import 'package:gutlay_etr_mad/widget/dialog_indicator/dialogs.dart';
 import 'package:hive/hive.dart';
@@ -9,26 +12,18 @@ class LetterFunction extends ChangeNotifier {
   final List<String> _handler = [];
 
 //? Opening the created HiveBox
-  final fourPicsOneword_box = Hive.box("4pics1Word_Box");
 
-  int _stageNo = 0;
+  int _stageNo = DatabaseHelper.getLevelIndex();
   final int _percentage = 0;
 
-  int levelIndex() {
-    fourPicsOneword_box.put('level', _stageNo);
-
-    // _stageNo = fourPicsOneword_box.get('level');
-
-    return _stageNo;
-  }
-
-  int get stageindex => levelIndex();
+  int get stageindex => _stageNo;
   List<String> get handler => _handler;
   List<LevelsData> get leveldata => _leveldata;
 
   //? PARA MA UPDATE STAGE
   void updateBoard({required int currentIndex}) {
     _stageNo = (_stageNo == 14) ? 0 : ++_stageNo;
+    DatabaseHelper.setlevelIndex(stageIndex: _stageNo);
     notifyListeners();
   }
 
