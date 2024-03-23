432db5033e46ac237b2f75edc7e743d8929b8008
Livsn06 <joelgutlau04@gmail.com>
2024-03-23 21:52:15 +0800

    joel add

--- lib/helpers/dbase_hive.dart
+++ lib/helpers/dbase_hive.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:hive/hive.dart';
 
 class DatabaseHelper {
   static final fourPicsOneword_box = Hive.box("4pics1Word_Box");
-
   //
   static void setlevelIndex({required int stageIndex}) {
     fourPicsOneword_box.put('level', stageIndex);
